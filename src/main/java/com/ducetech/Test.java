a93faec6a914e38f9d8d92145b26a163048970a0
lenzhao <lenzhao@yahoo.com>
2017-06-15 15:34:33 +0800

    int project

---
+++ src/main/java/com/ducetech/Test.java
@@ -0,0 +1,10 @@
+package com.ducetech;
+
+/**
+ * Created by lenzhao on 17-6-15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
